6e463bcd739662d77a3d8067bd9174ae01bf8572
DanyaKurbatov <66355412+DanyaKurbatov@users.noreply.github.com>
2020-06-09 18:54:31 +0300

    Initial commit

---
+++ main_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    file = models.FileField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
